128b21a1d084139d1309fe6fa1c692cccdd71abe
Fernando de Abreu <abreufea@gmail.com>
2023-08-16 13:11:32 -0300

    Primeiros passos do curso de cypress

---
+++ cypress/e2e/contact-us.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe("Test Contaact Us From via WebdriverUni", () =>  {
+    it ("Shoulb be able to submit a successful submission via constact us form", () => {
+    cy.visit ("https://webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.get('[name="first_name"]').type("joe");
+    cy.get('[name="last_name"]').type("Hermano");
+    cy.get('[name="email"]').type("hermano_joe@email.com");
+    cy.get('textarea.feedback-input').type("My first test alone...:)");
+    cy.get('[type="submit"]').click();
+});
+
+it ("Shoulb not be able to submit a successful submission via constact us form as all fields are required", () => {
+    });
+})
