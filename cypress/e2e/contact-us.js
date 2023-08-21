f19b129a117c160126f71405b09d638a1af8bf79
Fernando de Abreu <abreufea@gmail.com>
2023-08-21 10:44:35 -0300

    ATUALIZACAO 21

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -11,5 +11,10 @@ describe("Test Contaact Us From via WebdriverUni", () =>  {
 });
 
 it ("Shoulb not be able to submit a successful submission via constact us form as all fields are required", () => {
+    cy.visit ("https://webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.get('[name="first_name"]').type("joe");
+    cy.get('[name="last_name"]').type("Hermano");
+    cy.get('textarea.feedback-input').type("My first test alone...:)");
+    cy.get('[type="submit"]').click();
     });
 })
