c0ed280c222d4e7bebf3b243f657c1af84ceafb7
Fernando de Abreu <abreufea@gmail.com>
2023-08-31 17:47:55 -0300

    Handle js alerts

--- cypress/integration/webdriver-uni/browser-navegation.js
+++ cypress/integration/webdriver-uni/browser-navegation.js
@@ -14,10 +14,18 @@ describe("Validate webdriveruni homepage links", () => {
 
       cy.go('forward')
       cy.url().should('include', 'contactus')
-
+     
       cy.go('back')
+
       cy.get('#login-portal').invoke('removeAttr', 'target').click({force:true})
       cy.url().should('include', 'Login-Portal')
+      
       cy.go('back')
+      
+       cy.get('#to-do-list').invoke('removeAttr', 'target').click({force:true})
+       cy.url().should('include', 'To-Do-List')
+       
+       cy.go('back')
+
   });
 })
---
+++ cypress/integration/webdriver-uni/js-alerts.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+
+describe("Handle js alerts", () => {
+    it("Confirm js alert contains the correct text", () => {
+        //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#popup-alerts').invoke('removeAttr', 'target').click({force:true})
+
+        cy.get('#button1').click()
+
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('I am an alert box!')
+        })
+    });
+    it("Validate js confirm alert box works correctly when clicking ok", () => {
+        //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#popup-alerts').invoke('removeAttr', 'target').click({force:true})
+
+        cy.get('#button4').click()
+
+        cy.on('window:alert', (str) => {
+            return true;
+        })
+        cy.get('#confirm-alert-text').contains('You pressed OK!')
+    });
+    it("Validate js confirm alert box works correctly when clicking Cancel", () => {
+        //cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#popup-alerts').invoke('removeAttr', 'target').click({force:true})
+
+        cy.get('#button4').click()
+
+        cy.on('window:confirm', (str) => {
+            return false;
+        })
+        cy.get('#confirm-alert-text').contains('You pressed Cancel!')
+    });
+})
+
