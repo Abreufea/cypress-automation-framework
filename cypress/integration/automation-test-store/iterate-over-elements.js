7d68acc6e2ce274258870d56a35112fc3807dcb5
Fernando de Abreu <abreufea@gmail.com>
2023-09-05 12:45:52 -0300

    Improve test

--- cypress/integration/automation-test-store/iterate-over-elements.js
+++ cypress/integration/automation-test-store/iterate-over-elements.js
@@ -1,22 +1,24 @@
 /// <reference types="cypress" />
 
 describe("Iterate over elements", () => {
-    it("Log information of all hair care products", () => {
-      cy.visit("https://automationteststore.com/");
-      cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
-  
-      cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
-          cy.log("Index: " + index + " : " + $el.text())
-      });
-    });
-    it("Add specific product to basket", () => {
-      cy.visit("https://automationteststore.com/");
-      cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
-      
-      cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
-        if($el.text().includes('Curls to straight Shampoo')) {
-          cy.wrap($el).click()
-        }
-    });
+  beforeEach(function() {
+    cy.visit("https://automationteststore.com/");
+    cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
+  })
+  it("Log information of all hair care products", () => {
+    cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
+        cy.log("Index: " + index + " : " + $el.text())
     });
   });
+  it("Add specific product to basket", () => {
+    cy.selectProduct('Curls to straight Shampoo');
+  });
+
+  it("Add another specific product to basket", () => {
+    cy.selectProduct('Seaweed Conditioner');
+  });
+
+  it("Add another specific product to basket 2", () => {
+    cy.selectProduct('Eau Parfumee au The Vert Shampoo');
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,16 +10,23 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+// Cypress.Commands.add("login", (email, password) => { ... })
 //
 //
 // -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
 //
 //
 // -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+Cypress.Commands.add("selectProduct", productName => {
+    cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
+        if($el.text().includes(productName)) {
+            cy.wrap($el).click()
+        }
+    });
+})
+
 // -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import 'cypress-file-upload';
