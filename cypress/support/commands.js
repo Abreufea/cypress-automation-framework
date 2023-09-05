2149882d36b5e841ea3192ce9ef9ec986cc997ec
Fernando de Abreu <abreufea@gmail.com>
2023-09-05 15:30:16 -0300

    n

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,6 +25,15 @@ Cypress.Commands.add("selectProduct", productName => {
             cy.wrap($el).click()
         }
     });
+});
+
+Cypress.Commands.add("webdriverUni_ContactForm_Submission", (firstName, lastName, email, comment, $selector, textToLocate) => {
+    cy.get('[name="first_name"]').type(firstName);
+    cy.get('[name="last_name"]').type(lastName);
+    cy.get('[name="email"]').type(email)
+    cy.get('textarea.feedback-input').type(comment)
+    cy.get('[type="submit"]').click();
+    cy.get($selector).contains(textToLocate)
 })
 
 // -- This will overwrite an existing command --
