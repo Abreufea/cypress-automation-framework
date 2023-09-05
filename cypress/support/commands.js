b8bfc67fb4dba6e5932500cc5f4c6558eec456be
Fernando de Abreu <abreufea@gmail.com>
2023-09-05 17:00:33 -0300

    Command defeituoso

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,16 +10,42 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+// Cypress.Commands.add("login", (email, password) => { ... })
 //
 //
 // -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
 //
 //
 // -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+Cypress.Commands.add("selectProduct", productName => {
+    cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
+        if($el.text().includes(productName)) {
+            cy.wrap($el).click()
+        }
+    });
+});
+
+Cypress.Commands.add("addProductToBasket", productName => {
+    cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
+        if($el.text() === productName) {
+            cy.log($el.text())
+            cy.get('.productcart').eq(index).click();
+        }
+    });
+});
+
+
+Cypress.Commands.add("webdriverUni_ContactForm_Submission", (firstName, lastName, email, comment, $selector, textToLocate) => {
+    cy.get('[name="first_name"]').type(firstName);
+    cy.get('[name="last_name"]').type(lastName);
+    cy.get('[name="email"]').type(email)
+    cy.get('textarea.feedback-input').type(comment)
+    cy.get('[type="submit"]').click();
+    cy.get($selector).contains(textToLocate)
+})
+
 // -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import 'cypress-file-upload';
