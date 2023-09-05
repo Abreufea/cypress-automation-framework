d1cf41383462396cc859b8f2adcb869682ebc49d
Fernando de Abreu <abreufea@gmail.com>
2023-09-05 11:50:54 -0300

    Improve tests wife beforeEach

--- cypress/integration/webdriver-uni/checkboxes.js
+++ cypress/integration/webdriver-uni/checkboxes.js
@@ -1,10 +1,11 @@
 /// <reference types="cypress" />
 
 describe("Verify checkboxes via webdriveruni", () => {
-    it("Check and validate checkbox", () => {
+    beforeEach(function() {
         cy.visit("http://www.webdriveruniversity.com")
         cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
-
+    })
+    it("Check and validate checkbox", () => {
         //cy.get('#checkboxes > :nth-child(1) > input').check()
         //cy.get('#checkboxes > :nth-child(1) > input').check().should('be.checked')
 
@@ -14,17 +15,11 @@ describe("Verify checkboxes via webdriveruni", () => {
     });
 
     it("Uncheck and validate checkbox", () => {
-        cy.visit("http://www.webdriveruniversity.com")
-        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
-
         cy.get(':nth-child(5) > input').as('option-3')
         cy.get('@option-3').uncheck().should('not.be.checked')
     });
 
-    it("Check multiple checkboxes", () => {
-        cy.visit("http://www.webdriveruniversity.com")
-        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force:true})
-
+    it("Check mutiple checkboxes", () => {
         cy.get("input[type='checkbox']").check(["option-1", "option-2", "option-3", "option-4"]).should('be.checked')
     });
 })
