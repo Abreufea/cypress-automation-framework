c6be06913f8edfbeaf571d0083d3935a4e571c51
Fernando de Abreu <abreufea@gmail.com>
2023-09-05 15:33:15 -0300

    comand

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,6 +27,16 @@ Cypress.Commands.add("selectProduct", productName => {
     });
 });
 
+Cypress.Commands.add("addProductToBasket", productName => {
+    cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
+        if($el.text() === productName) {
+            cy.log($el.text())
+            cy.get('.productcart').eq(index).click();
+        }
+    });
+});
+
+
 Cypress.Commands.add("webdriverUni_ContactForm_Submission", (firstName, lastName, email, comment, $selector, textToLocate) => {
     cy.get('[name="first_name"]').type(firstName);
     cy.get('[name="last_name"]').type(lastName);
