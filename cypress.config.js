071542c10295e3835593b220bd03c3f8aa8e2a57
Fernando de Abreu <abreufea@gmail.com>
2023-09-07 11:46:29 -0300

    Melhora nos testes

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,17 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern:"cypress/integration/**/*.{js, jsx, ts, tsx, feature}"
-    
+    specPattern: "cypress/integration/**/*.{js, jsx, ts, tsx, feature}",
+    baseUrl: "http://www.webdriveruniversity.com",
+    chromeWebSecurity: false,
+    experimentalSessionAndOrigen: true,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 120000,
+    env: {
+      first_name: "Sarah",
+      webdriveruni_homepage: "http://www.webdriveruniversity.com"
+    }
+
   },
 
 });
--- cypress/integration/webdriver-uni/autocomplete-dropdown-list.js
+++ cypress/integration/webdriver-uni/autocomplete-dropdown-list.js
@@ -2,7 +2,7 @@
 
 describe("Verify Autocomplete dropwdown lists via webdriveruni", () => {
     it("Select specific product via autocomplete list", () => {
-        cy.visit("http://www.webdriveruniversity.com")
+        cy.visit("/")
         cy.get('#autocomplete-textfield').invoke('removeAttr', 'target').click({force:true})
 
         cy.get('#myInput').type('A')
