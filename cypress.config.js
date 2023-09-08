7738971e595eca457628b219e4cf15fe8d1c042d
Fernando de Abreu <abreufea@gmail.com>
2023-09-08 14:27:22 -0300

    reporter config

--- cypress.config.js
+++ cypress.config.js
@@ -12,6 +12,16 @@ module.exports = defineConfig({
     experimentalSessionAndOrigin: true,
     defaultCommandTimeout: 10000,
     pageLoadTimeout: 120000,
+    screenshotOnRunFailure: true,
+    trashAssetsBeforeRuns: true,
+    video: false,
+    videoUploadOnPasses: false,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      configFile: 'reporter-config.json',
+    },
     env: {
       first_name: "Sarah",
       webdriveruni_homepage: "http://www.webdriveruniversity.com"
--- cypress/support/pageObjects/automation-test-store/AutoStore_HairCare_PO.js
+++ cypress/support/pageObjects/automation-test-store/AutoStore_HairCare_PO.js
@@ -1,7 +1,9 @@
 class AutoStore_HairCare_Po {
     addHairCareProductsToBasket() {
         globalThis.data.productName.forEach(function(element) {
-            cy.addProductToBasket(element)
+            cy.addProductToBasket(element).then(() => {
+                //debugger
+            })
         })
         cy.get('.dropdown-toggle > .fa').click();
     }
